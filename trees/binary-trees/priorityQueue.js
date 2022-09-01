4eae153bd7f076e36b5d40c115fe62146b675d9e
Hagay Haut <hagayhaut@gmail.com>
2022-08-31 20:27:26 -0400

    add: priority queue

---
+++ trees/binary-trees/priorityQueue.js
@@ -0,0 +1,94 @@
+// PRIORITY QUEUE
+
+// abstract structure, often made with heaps
+
+// This priority queue uses a min binary-heap
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+
+  // like heap insert:
+  // 1. put at end bubble
+  // 2. bubble up according to priority
+  // O(log n)
+  enqueue(val, priority) {
+    const newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      const parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+
+  // removes root, switch with last
+  // sink down new root according to priority
+  // O(log n)
+  dequeue() {
+    const min = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return min;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      const leftChildIdx = 2 * idx + 1;
+      const rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild.priority < element.priority) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (!swap && rightChild.priority < element.priority) ||
+          (swap && rightChild.priority < leftChild.priority)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (!swap) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
+}
+
+const ER = new PriorityQueue();
+
+ER.enqueue("cold", 5);
+ER.enqueue("gun-shot", 1);
+ER.enqueue("covid", 3);
+ER.enqueue("broken arm", 2);
+ER.enqueue("glass in foot", 4);
